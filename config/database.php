b3622624c4d8f8967647c9ffdd63667a5b718116
Renê Milagres <renevictorm@gmail.com>
2019-08-25 17:07:06 -0300

    :sparkles: Configuração da conexão com o bd

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+class Database{
+ 
+    // especificando as credenciais
+    private $host = "192.168.99.100:3306";
+    private $db_name = "biblioteca";
+    private $username = "root";
+    private $password = "root";
+    public $conn;
+ 
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
